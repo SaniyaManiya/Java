50641fa412fa269ba55dd9b5b646c6170d86ec3e
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-10 14:57:57 +0530

    Fix swapping logic in SwapUsingThree class

---
+++ Basics/SwapUsingThree.java
@@ -0,0 +1,12 @@
+public class SwapUsingThree{
+  public static void main(String[] args){
+    int x= 100;
+    int y= 200;
+    int temp=x;
+    x=y;
+    int y=temp;
+    System.out.println("After Swap: ");
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+  }
+}
