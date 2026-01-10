7b0ae425f14d140d4eff2d6d38a184044c609438
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-10 15:11:49 +0530

    Add XOR swap implementation in SwapUsingXOR.java

---
+++ Basics/SwapUsingXOR.java
@@ -0,0 +1,11 @@
+public class SwapUsingXOR{
+  public static void main(String args[]){
+    int a = 10;
+    int b = 20;
+    System.out.println("Before Swap , a = " + a + " and b = " + b);
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    System.out.println("After Swap , a = " + a + " and b = " + b);
+  }
+}
