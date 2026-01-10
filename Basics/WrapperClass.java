b04d2a573c8c71636b625b503f771325615a0fd9
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-10 15:16:56 +0530

    Add WrapperClass to demonstrate boxing in Java

---
+++ Basics/WrapperClass.java
@@ -0,0 +1,11 @@
+public class WrapperClass{
+  public static void main(String args[]){
+    int b;
+    Integer a;
+    b = 137;
+    a = b;
+    System.out.println("The primitive int b is : " + b);
+    System.out.println("The Integer obj a is : " + a);
+  }
+}
+    
