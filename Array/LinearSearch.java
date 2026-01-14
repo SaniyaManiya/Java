8b6569eb8bfb4ddd15fa64656f5bc462f064ed7d
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-14 20:48:45 +0530

    Add LinearSearch class for element search

---
+++ Array/LinearSearch.java
@@ -0,0 +1,16 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int key = 30;
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println(found ? "Element found" : "Element not found");
+    }
+}
