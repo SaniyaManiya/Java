362391eb1f8594fd9702042f9cbc0458b6429994
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-15 21:59:36 +0530

    Add CopyArray class to copy an array

---
+++ Array/CopyArray.java
@@ -0,0 +1,14 @@
+public class CopyArray {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        int[] b = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+
+        System.out.println("Copied array:");
+        for (int n : b)
+            System.out.print(n + " ");
+    }
+}
