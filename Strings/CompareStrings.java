b5c5a6aa8495701563a15e719873ea05c8b5d1ca
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-17 15:26:30 +0530

    Create CompareStrings.java

---
+++ Strings/CompareStrings.java
@@ -0,0 +1,10 @@
+public class CompareStrings{
+  public static void main(String args[]){
+    String a = "java";
+    String b = "java";
+    if(a.equal(b))
+      System.out.println("Equal");
+    else
+      System.out.println("Not Equal");
+  }
+}
