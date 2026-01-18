2578f3263fd9222e0c18e411595fe5525ffe1a1e
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-18 18:15:29 +0530

    Create Pattern9.java

---
+++ Pattern/TrianglePattern/Pattern9.java
@@ -0,0 +1,12 @@
+class Pattern9 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            char ch = 'A';
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
