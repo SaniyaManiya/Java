9c48b61b11e08e4f20f10edc95e916193c247f4e
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-24 18:29:27 +0530

    Create FullStarPyramid.java

---
+++ Pattern/PyramidPattern/FullStarPyramid.java
@@ -0,0 +1,14 @@
+public class FullStarPyramid {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
