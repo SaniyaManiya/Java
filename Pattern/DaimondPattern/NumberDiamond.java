0d78227ce2e71556d55a7b4821ea976d09dcca6a
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-26 17:37:44 +0530

    Create NumberDiamond.java

---
+++ Pattern/DaimondPattern/NumberDiamond.java
@@ -0,0 +1,19 @@
+public class NumberDiamond {
+    public static void main(String[] args) {
+        int n = 4;
+
+        for (int i = 1; i <= n; i++) {
+            for (int s = n; s > i; s--) System.out.print(" ");
+            for (int j = i; j >= 1; j--) System.out.print(j);
+            for (int j = 2; j <= i; j++) System.out.print(j);
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int s = n; s > i; s--) System.out.print(" ");
+            for (int j = i; j >= 1; j--) System.out.print(j);
+            for (int j = 2; j <= i; j++) System.out.print(j);
+            System.out.println();
+        }
+    }
+}
