8a3d3d7c32c3a53da8000ee69f0e8c2ed173702b
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-01-27 18:57:54 +0530

    Create methodOverriding.java

---
+++ OOPs/methodOverriding.java
@@ -0,0 +1,16 @@
+class Bank {
+    int getRate() {
+        return 5;
+    }
+}
+
+class SBI extends Bank {
+    int getRate() {
+        return 7;
+    }
+
+    public static void main(String[] args) {
+        Bank b = new SBI();
+        System.out.println(b.getRate());
+    }
+}
