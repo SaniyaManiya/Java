a306ea298b96fbe84572023abca28c45c39ef819
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-02 19:17:03 +0530

    Add Weekday or Weekend switch statement

---
+++ Loops/Switch/WeekdayorWeekend.java
@@ -0,0 +1,10 @@
+int day = 6;
+
+switch (day) {
+    case 1: case 2: case 3: case 4: case 5:
+        System.out.println("Weekday"); break;
+    case 6: case 7:
+        System.out.println("Weekend"); break;
+    default:
+        System.out.println("Invalid day");
+}
