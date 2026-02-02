f8061310d542afc054da31e0f99428b790d95e78
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-02 19:15:45 +0530

    Add TrafficSignal.java with switch statement

---
+++ Loops/Switch/TrafficSignal.java
@@ -0,0 +1,8 @@
+String signal = "RED";
+
+switch (signal) {
+    case "RED": System.out.println("Stop"); break;
+    case "YELLOW": System.out.println("Ready"); break;
+    case "GREEN": System.out.println("Go"); break;
+    default: System.out.println("Invalid signal");
+}
