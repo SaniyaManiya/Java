6eacaeaaaed6a43fea4837e3884ba7e0db846537
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-02 19:16:22 +0530

    Create MenuDriven.java

---
+++ Loops/Switch/MenuDriven.java
@@ -0,0 +1,8 @@
+int choice = 2;
+
+switch (choice) {
+    case 1: System.out.println("Check Balance"); break;
+    case 2: System.out.println("Withdraw Money"); break;
+    case 3: System.out.println("Deposit Money"); break;
+    default: System.out.println("Invalid choice");
+}
