eaeff5ceb954477b812123e466dd7b515fa21464
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-02 19:13:14 +0530

    Add SimpleCalculator.java for basic arithmetic operations

---
+++ Loops/Switch/SimpleCalculator.java
@@ -0,0 +1,10 @@
+int a = 10, b = 5;
+char op = '+';
+
+switch (op) {
+    case '+': System.out.println(a + b); break;
+    case '-': System.out.println(a - b); break;
+    case '*': System.out.println(a * b); break;
+    case '/': System.out.println(a / b); break;
+    default: System.out.println("Invalid operator");
+}
