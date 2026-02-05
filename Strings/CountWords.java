c098ffe3cdc2ff3017393f9f978085a2f53840f8
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-05 20:19:47 +0530

    Create CountWords.java

---
+++ Strings/CountWords.java
@@ -0,0 +1,7 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String s = "I love Java programming";
+        String[] words = s.split(" ");
+        System.out.println("Words: " + words.length);
+    }
+}
