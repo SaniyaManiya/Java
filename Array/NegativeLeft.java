3578b12385fcc0fd7469f19fa32ba13c4cd0cd26
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-06 21:01:13 +0530

    Create NegativeLeft.java

---
+++ Array/NegativeLeft.java
@@ -0,0 +1,13 @@
+class NegativeLeft {
+    public static void main(String[] args) {
+        int[] a={1,-2,3,-4,5};
+
+        for(int i=0;i<a.length;i++)
+            if(a[i]<0)
+                System.out.print(a[i]+" ");
+
+        for(int i=0;i<a.length;i++)
+            if(a[i]>=0)
+                System.out.print(a[i]+" ");
+    }
+}
