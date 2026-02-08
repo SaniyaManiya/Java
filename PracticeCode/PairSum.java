deecd51cbc8567e5e30a3b932952f6dc2f57c941
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-08 23:52:38 +0530

    Create PairSum.java

---
+++ PracticeCode/PairSum.java
@@ -0,0 +1,14 @@
+public class PairSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 5, 6};
+        int target = 10;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    System.out.println(arr[i] + " " + arr[j]);
+                }
+            }
+        }
+    }
+}
