a8947dad2fd78f445cb21e7de92899794f480daf
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-08 23:49:23 +0530

    Create SmallestElement.java

---
+++ PracticeCode/SmallestElement.java
@@ -0,0 +1,13 @@
+public class SmallestElement {
+    public static void main(String[] args) {
+        int[] arr = {7, 3, 9, 2, 5};
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Smallest element: " + min);
+    }
+}
