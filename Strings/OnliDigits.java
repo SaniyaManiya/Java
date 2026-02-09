6649645ffe6ea99b5186c2e28b30276f776c85da
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-09 16:51:42 +0530

    Create OnliDigits.java

---
+++ Strings/OnliDigits.java
@@ -0,0 +1,15 @@
+public class OnlyDigits {
+    public static void main(String[] args) {
+        String str = "12345";
+        boolean isDigit = true;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                isDigit = false;
+                break;
+            }
+        }
+
+        System.out.println(isDigit ? "Only Digits" : "Contains Other Characters");
+    }
+}
