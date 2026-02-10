d9f4e446cfece96dab817368c4c31189419c7dd3
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-10 23:49:53 +0530

    Create FirstRepeating.java

---
+++ Array/FirstRepeating.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class FirstRepeating {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 3, 4, 3, 5, 6};
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (set.contains(arr[i])) {
+                System.out.println(arr[i]);
+            }
+            set.add(arr[i]);
+        }
+    }
+}
