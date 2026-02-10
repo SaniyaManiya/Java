a4c3ab05b50aedd74fbc9aa7e638658fa83064ed
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-10 23:47:58 +0530

    Create ArrayUnion.java

---
+++ Array/ArrayUnion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ArrayUnion {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        int[] b = {3, 4, 5};
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : a) set.add(i);
+        for (int i : b) set.add(i);
+
+        System.out.println(set);
+    }
+}
