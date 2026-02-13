28ee1592a3a4df39b8933dd04ae41df4690f448a
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-13 10:16:59 +0530

    Create InorderTraversal.java

---
+++ DSA/InorderTraversal.java
@@ -0,0 +1,22 @@
+class TreeNode{
+    int val;
+    TreeNode left,right;
+    TreeNode(int v){val=v;}
+}
+
+public class InorderTraversal {
+    static void inorder(TreeNode root){
+        if(root==null) return;
+        inorder(root.left);
+        System.out.print(root.val+" ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args){
+        TreeNode root=new TreeNode(1);
+        root.right=new TreeNode(2);
+        root.right.left=new TreeNode(3);
+
+        inorder(root);
+    }
+}
