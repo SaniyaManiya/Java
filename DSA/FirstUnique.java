82776e1bf7ce25dd3c2cfd32d63dadc8097bf0ae
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-13 10:15:07 +0530

    Create FirstUnique.java

---
+++ DSA/FirstUnique.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class FirstUnique {
+    public static void main(String[] args) {
+        String s="swiss";
+        LinkedHashMap<Character,Integer> map=new LinkedHashMap<>();
+
+        for(char c:s.toCharArray())
+            map.put(c,map.getOrDefault(c,0)+1);
+
+        for(char c:map.keySet()){
+            if(map.get(c)==1){
+                System.out.println(c);
+                break;
+            }
+        }
+    }
+}
