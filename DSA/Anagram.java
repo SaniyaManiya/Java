8d9fb96525cc9723cdb2d86a016d2dfd297af704
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-13 10:12:00 +0530

    Create Anagram.java

---
+++ DSA/Anagram.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String a="listen";
+        String b="silent";
+
+        char[] x=a.toCharArray();
+        char[] y=b.toCharArray();
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        System.out.println(Arrays.equals(x,y));
+    }
+}
