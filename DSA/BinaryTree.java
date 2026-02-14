18eb16d5cbb395989786fc1c6416fefbaf40d95d
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-14 12:59:35 +0530

    Create BinaryTree.java

---
+++ DSA/BinaryTree.java
@@ -0,0 +1,21 @@
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){data=d;}
+}
+
+public class TreeHeight {
+
+    static int height(Node root){
+        if(root==null) return 0;
+        return 1+Math.max(height(root.left),height(root.right));
+    }
+
+    public static void main(String[] args){
+        Node root=new Node(1);
+        root.left=new Node(2);
+        root.right=new Node(3);
+
+        System.out.println(height(root));
+    }
+}
