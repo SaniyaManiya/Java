dc50e10beb20cf9b08135d175069683e2c7bfdb5
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-16 00:10:09 +0530

    Create HeapSort.java

---
+++ DSA/HeapSort.java
@@ -0,0 +1,30 @@
+public class HeapSort {
+
+    static void heapify(int[] a,int n,int i){
+        int largest=i,l=2*i+1,r=2*i+2;
+
+        if(l<n && a[l]>a[largest]) largest=l;
+        if(r<n && a[r]>a[largest]) largest=r;
+
+        if(largest!=i){
+            int t=a[i];a[i]=a[largest];a[largest]=t;
+            heapify(a,n,largest);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] a={4,10,3,5,1};
+        int n=a.length;
+
+        for(int i=n/2-1;i>=0;i--)
+            heapify(a,n,i);
+
+        for(int i=n-1;i>0;i--){
+            int t=a[0];a[0]=a[i];a[i]=t;
+            heapify(a,i,0);
+        }
+
+        for(int x:a)
+            System.out.print(x+" ");
+    }
+}
