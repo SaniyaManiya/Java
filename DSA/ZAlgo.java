eed281bbb59835fb1345c490f11cc7504f6a146a
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-18 15:09:29 +0530

    Create ZAlgo.java

---
+++ DSA/ZAlgo.java
@@ -0,0 +1,16 @@
+public class ZAlgo {
+    public static void main(String[] args){
+        String s="aabxaabxcaabxaabxay";
+        int n=s.length();
+        int[] z=new int[n];
+
+        for(int i=1,l=0,r=0;i<n;i++){
+            if(i<=r) z[i]=Math.min(r-i+1,z[i-l]);
+            while(i+z[i]<n && s.charAt(z[i])==s.charAt(i+z[i]))
+                z[i]++;
+            if(i+z[i]-1>r){ l=i; r=i+z[i]-1; }
+        }
+
+        for(int x:z) System.out.print(x+" ");
+    }
+}
