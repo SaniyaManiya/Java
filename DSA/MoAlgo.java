3e1c919aafb8ed739caaefda35a2f89e5186b48f
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-22 13:00:35 +0530

    Create MoAlgo.java

---
+++ DSA/MoAlgo.java
@@ -0,0 +1,12 @@
+public class MoAlgo {
+    static class Query{
+        int l,r;
+        Query(int l,int r){this.l=l;this.r=r;}
+    }
+
+    public static void main(String[] args){
+        // Skeleton for Mo's Algorithm
+        // Sort queries by blocks
+        System.out.println("Mo's Algorithm template");
+    }
+}
