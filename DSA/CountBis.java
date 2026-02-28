c63f3044f8788ab77a236707367066ef85295964
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-28 12:41:50 +0530

    Create CountBis.java

---
+++ DSA/CountBis.java
@@ -0,0 +1,10 @@
+public class CountBits {
+    public static void main(String[] args){
+        int n=29,count=0;
+        while(n>0){
+            n&=(n-1);
+            count++;
+        }
+        System.out.println(count);
+    }
+}
