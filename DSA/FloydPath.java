9834a23615605ae8b28701379febccf3cc6eee28
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-02-28 12:46:23 +0530

    Create FloydPath.java

---
+++ DSA/FloydPath.java
@@ -0,0 +1,25 @@
+public class FloydPath {
+    static final int INF = 9999;
+
+    public static void main(String[] args){
+        int[][] g = {
+            {0,3,INF},
+            {2,0,INF},
+            {INF,1,0}
+        };
+
+        int V = g.length;
+
+        for(int k=0;k<V;k++)
+            for(int i=0;i<V;i++)
+                for(int j=0;j<V;j++)
+                    if(g[i][k] + g[k][j] < g[i][j])
+                        g[i][j] = g[i][k] + g[k][j];
+
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++)
+                System.out.print(g[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
