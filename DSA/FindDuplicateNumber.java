17499bbc137ef8872d1a505ea032325d65087773
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-03-14 12:28:42 +0530

    Create FindDuplicateNumber.java

---
+++ DSA/FindDuplicateNumber.java
@@ -0,0 +1,24 @@
+public class FindDuplicateNumber {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,3,4,2,2};
+
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        System.out.println("Duplicate Number: " + slow);
+    }
+}
