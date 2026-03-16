a29735a96bd1ef35251f7e62de6fa6b9d2b8c20c
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-03-16 17:08:11 +0530

    Implement Product Array Except Self algorithm

---
+++ ProductArrayExceptSelf.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ProductArrayExceptSelf {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4};
+        int n = nums.length;
+
+        int[] result = new int[n];
+
+        int prefix = 1;
+        for(int i = 0; i < n; i++){
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int suffix = 1;
+        for(int i = n-1; i >= 0; i--){
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        System.out.println(Arrays.toString(result));
+    }
+}
