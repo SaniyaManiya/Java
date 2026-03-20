2ac6c885dd45e26744c0210f0cc9a991c41219ba
Saniya Maniyar <maniyarsaniya49@gmail.com>
2026-03-20 19:09:43 +0530

    Implement binary search in 2D matrix

---
+++ DSA/Search2DMatrix.java
@@ -0,0 +1,38 @@
+public class Search2DMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1,3,5,7},
+            {10,11,16,20},
+            {23,30,34,60}
+        };
+
+        int target = 3;
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int left = 0;
+        int right = m * n - 1;
+
+        while(left <= right){
+
+            int mid = (left + right) / 2;
+
+            int value = matrix[mid / n][mid % n];
+
+            if(value == target){
+                System.out.println("Found");
+                return;
+            }
+
+            else if(value < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        System.out.println("Not Found");
+    }
+}
